a282c64556c0ddb7bbeb38210c0262dcdba0e1a1
eymeric <eymericdechelette@gmail.com>
2024-01-27 17:13:45 +0100

    bezier tests

--- src/bezier_mirror.rs
+++ src/bezier_mirror.rs
@@ -1,8 +1,10 @@
 use nalgebra::{Point, SMatrix, SVector, Unit};
+use std::io::Write;
 
 use crate::{mirror::Mirror, ray::Ray, DIM};
 
-struct BezierMirror {
+#[derive(PartialEq, Debug)]
+pub struct BezierMirror {
     control_points: Vec<Point<f32, DIM>>,
 }
 
@@ -93,4 +95,166 @@ fn binomial_coefficient(n: usize, k: usize) -> usize {
     }
 
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use super::*;
+
+    fn complete_with_0(mut vec: Vec<f32>) -> Vec<f32> {
+        vec.resize(DIM, 0.0);
+        vec
+    }
+
+    #[test]
+    fn test_binomial_coefficient() {
+        assert_eq!(binomial_coefficient(0, 0), 1);
+        assert_eq!(binomial_coefficient(1, 0), 1);
+        assert_eq!(binomial_coefficient(1, 1), 1);
+        assert_eq!(binomial_coefficient(2, 0), 1);
+        assert_eq!(binomial_coefficient(2, 1), 2);
+        assert_eq!(binomial_coefficient(2, 2), 1);
+        assert_eq!(binomial_coefficient(3, 0), 1);
+        assert_eq!(binomial_coefficient(3, 1), 3);
+        assert_eq!(binomial_coefficient(3, 2), 3);
+        assert_eq!(binomial_coefficient(3, 3), 1);
+        assert_eq!(binomial_coefficient(4, 0), 1);
+        assert_eq!(binomial_coefficient(4, 1), 4);
+        assert_eq!(binomial_coefficient(4, 2), 6);
+        assert_eq!(binomial_coefficient(4, 3), 4);
+        assert_eq!(binomial_coefficient(4, 4), 1);
+    }
+
+    #[test]
+    fn test_calculate_linear_point_2d() {
+        let bezier_mirror = BezierMirror {
+            control_points: vec![
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.0, 0.0])),
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![1.0, 1.0])),
+            ],
+        };
+        assert_eq!(
+            bezier_mirror.calculate_point(0.0),
+            Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.0, 0.0]))
+        );
+        assert_eq!(
+            bezier_mirror.calculate_point(0.5),
+            Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.5, 0.5]))
+        );
+        assert_eq!(
+            bezier_mirror.calculate_point(1.0),
+            Point::from_slice(&complete_with_0(vec![1.0, 1.0]))
+        );
+    }
+
+    #[test]
+    fn test_calculate_cubic_point_2d() {
+        let bezier_mirror = BezierMirror {
+            control_points: vec![
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.0, 0.0])),
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.5, 1.0])),
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![1.0, 0.0])),
+            ],
+        };
+        assert_eq!(
+            bezier_mirror.calculate_point(0.0),
+            Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.0, 0.0]))
+        );
+        assert_eq!(
+            bezier_mirror.calculate_point(0.5),
+            Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.5, 0.5]))
+        );
+        assert_eq!(
+            bezier_mirror.calculate_point(1.0),
+            Point::from_slice(&complete_with_0(vec![1.0, 0.0]))
+        );
+    }
+
+    #[test]
+    fn test_calculate_quadratic_point_2d() {
+        let bezier_mirror = BezierMirror {
+            control_points: vec![
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.0, 0.0])),
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.5, 0.0])),
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.5, 1.0])),
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![1.0, 1.0])),
+            ],
+        };
+        assert_eq!(
+            bezier_mirror.calculate_point(0.0),
+            Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.0, 0.0]))
+        );
+
+        assert_eq!(
+            bezier_mirror.calculate_point(0.5),
+            Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.5, 0.5]))
+        );
+
+        assert_eq!(
+            bezier_mirror.calculate_point(1.0),
+            Point::from_slice(&complete_with_0(vec![1.0, 1.0]))
+        );
+    }
+
+    #[test]
+    fn generate_point_in_csv() {
+        //simple function to visualize the bezier curve to check that I dont do shit
+        let bezier_mirror = BezierMirror {
+            control_points: vec![
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.0, 0.0])),
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.5, 1.0])),
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.0, 1.0])),
+            ],
+        };
+
+        let mut file = std::fs::File::create("points.csv").unwrap();
+        for i in 0..100 {
+            let t = i as f32 / 100.0;
+            let point = bezier_mirror.calculate_point(t);
+            writeln!(file, "{},{}", point[0], point[1]).unwrap();
+            println!("{} : {}", t, point);
+        }
+    }
+
+    #[test]
+    fn test_calculate_tangent() {
+        let bezier_mirror = BezierMirror {
+            control_points: vec![
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.0, 0.0])),
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![0.5, 1.0])),
+                Point::<f32, DIM>::from_slice(&complete_with_0(vec![1.0, 0.0])),
+            ],
+        };
+
+        let vector = bezier_mirror.calculate_tangent(1.0);
+        let axis = SVector::<f32, DIM>::from_vec(complete_with_0(vec![1.0, 0.0]));
+        let dot_product = vector.dot(&axis);
+        let reflected_vector = 2.0 * dot_product * axis - vector;
+
+        assert_eq!(bezier_mirror.calculate_tangent(0.0), reflected_vector);
+    }
+
+    #[test]
+    fn test_from_json() {
+        let json: HashMap<String, Vec<Vec<f32>>> = HashMap::from([(
+            "control_points".to_string(),
+            vec![
+                complete_with_0(vec![1.0, 2.0, 3.0]),
+                complete_with_0(vec![4.0, 5.0, 6.0]),
+                complete_with_0(vec![7.0, 8.0, 9.0]),
+            ],
+        )]);
+        assert_eq!(
+            BezierMirror::from_json(&serde_json::to_value(json).unwrap()),
+            BezierMirror {
+                control_points: vec![
+                    Point::<f32, DIM>::from_slice(&complete_with_0(vec![1.0, 2.0, 3.0])),
+                    Point::<f32, DIM>::from_slice(&complete_with_0(vec![4.0, 5.0, 6.0])),
+                    Point::<f32, DIM>::from_slice(&complete_with_0(vec![7.0, 8.0, 9.0])),
+                ],
+            }
+        );
+    }
 }
